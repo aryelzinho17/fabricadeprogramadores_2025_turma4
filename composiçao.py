556798531b47ee94eb00aa96c4e5234e96e55333
aryelzinho17 <aryelvirgolino969@gmail.com>
2025-09-05 17:37:47 -0300

    aula de composição
    exercícios de composição

---
+++ composiçao.py
@@ -0,0 +1,28 @@
+x=17
+print("aryel tem %d anos" %x)
+
+print("[%d]" % x)
+
+print("[%03d]" % x)
+
+print("[%3d]" % x)
+
+print("[%-3d]" % x)
+
+print("%5f" % x)
+
+print("%5.2f" % x)
+
+print("%10.5f" % x)
+
+nome = "aryel"
+
+idade = "17"
+
+grana = "20000"
+
+print("%s tem %d anos e R$%f no bolso."(nome, idade, grana))
+
+print("%12s tem %03d anos e R$%5.2f"(nome, idade, grana))
+
+print("%_12s tem %-3d anos e R$%-5.2f no bolso"(nome, idade, grana))
